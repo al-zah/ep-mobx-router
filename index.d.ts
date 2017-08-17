827c1cc8b231bd1f463057bd9a03a78c2ebc4259
o.zakharchenko <o.zakharchenko@engagepoint.com>
2017-08-17 16:15:11 +0300

    Add typings

---
+++ index.d.ts
@@ -0,0 +1,26 @@
+///<reference types="react" />
+
+type IRouteParams = any;
+
+type LifecyclePropsType = (context: IRoute, params: IRouteParams, store: any) => void;
+
+interface IRoute {
+    path: string;
+    component: React.ComponentClass | React.StatelessComponent | React.ReactElement<any> | null;
+    onEnter?: LifecyclePropsType;
+    onParamsChange?: LifecyclePropsType;
+    childRoutes?: { [key: string]: any };
+}
+
+export const RouterStore: {
+    new(): any;
+}
+
+export const Route: {
+    new(param: IRoute): any;
+    childRoutes?: { [key: string]: any };
+};
+type routerMap = { [key: string]: typeof Route };
+export const Link: React.ComponentClass<{ params: IRouteParams; view: any; store: any; className?: string; key?: string }>;
+export const MobxRouter: React.ComponentClass<{routes: {[key: string]: typeof Route}}>;
+export function startRouter(views: {[key: string]: typeof Route}, store: any): void;
